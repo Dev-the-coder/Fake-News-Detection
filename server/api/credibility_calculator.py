c4ca131975e16c6f062a21ce0119e596657ceb48
ayan <mukherjeeayan0813@gmail.com>
2024-06-17 18:31:49 +0530

    improved llm response

--- server/api/credibility_calculator.py
+++ server/api/credibility_calculator.py
@@ -3,7 +3,7 @@ import re
 
 def calculate_credibility(classifier_result, analysis, relevance_scores):
     analysis_answer = re.findall(r"\*\*(\d)\*\*", analysis)
-    weights = [0.5, 0.4, 0.2]
+    weights = [0.36, 0.52, 0.12]
     if not len(analysis_answer):
         analysis_answer = re.findall("fake", analysis)
         if not len(analysis_answer):
@@ -19,11 +19,18 @@ def calculate_credibility(classifier_result, analysis, relevance_scores):
     l = len(relevance_scores)
     if not l:
         weights[1] = 0
-    relevance_answer = sum(
-        [(l - i + 1) * x for i, x in enumerate(relevance_scores)]
-    ) / sum(list(range(1, l + 1)))
+        relevance_answer = 0
+    else:
+        relevance_answer = sum(
+            [(l - i + 1) * x for i, x in enumerate(relevance_scores)]
+        ) / sum(list(range(1, l + 1)))
     return (
-        weights[0] * analysis_answer
-        + relevance_answer * weights[1]
-        + classifier_result * weights[2]
+        (
+            weights[0] * analysis_answer
+            + relevance_answer * weights[1]
+            + classifier_result * weights[2]
+        ),
+        analysis_answer,
+        relevance_answer,
+        classifier_result,
     )
